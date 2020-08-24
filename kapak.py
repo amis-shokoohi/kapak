e356f432d04bb72eaee23448e2003ec2e671a776
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2020-08-24 12:23:33 +0430

    splited enc_n_dec.py into 3 modules

--- kapak.py
+++ kapak.py
@@ -5,7 +5,8 @@ from pathlib import Path
 
 from kpk_mods.passwd import getPassword, deriveKey
 from kpk_mods.progress import getTotalSize, showProgressComplete
-from kpk_mods.enc_n_dec import aesEncryptor, writeMeta, readMeta, encFile, decFile
+from kpk_mods.encryption import aesEncryptor, writeMeta, encryptFile
+from kpk_mods.decryption import aesDecryptor, readMeta, decryptFile
 from kpk_mods.constants import ENCRYPT_MODE, DECRYPT_MODE, TITLE, DESCRYPTION, USAGE, HELP_MESSAGE
 
 def hasRemoveFlag(argv):
@@ -80,82 +81,81 @@ def start():
 	rm = hasRemoveFlag(argv)
 
 	mode = whichMode(argv)
-	if mode != None:
-		p= Path(getPath(argv))
-		if p == None:
-			exit('\n Error: <path> variable NOT specified\n')
-		elif not os.path.exists(p):
-			exit('\n Error: Can NOT find ' + str(p) + '\n')
-
-		if os.path.isdir(p):
-			# Get all file paths in the directory recursively
-			fList = getFilesList(p, mode)
-			if len(fList) < 1:
-				exit('\n Error: ' + str(p) + ' is empty\n')
-
-			password = getPassword(mode)
-
-			if mode == ENCRYPT_MODE:
-				print('\n # Encrypting...\n')
-			else:
-				print('\n # Decrypting...\n')
-				
-			# Get total size of files in the directory
-			total_size = getTotalSize(fList)
-
-			if mode == ENCRYPT_MODE:
-				(key, salt) = deriveKey(password, None)
-				for f in fList:
-					if os.path.isdir(f):
-						continue
-					
-					(encryptor, iv) = aesEncryptor(key)
-					encryptor = writeMeta(encryptor, f, iv, salt)
-					encFile(encryptor, f, total_size)
-
-					if rm:
-						os.remove(f)
-			else:
-				for f in fList:
-					if os.path.isdir(f):
-						continue
-
-					(decryptor, f_out_ext) = readMeta(f, password)
-					decFile(decryptor, f, total_size, f_out_ext)
-
-					if rm:
-						os.remove(f)
-		elif os.path.isfile(p):
-			if mode == ENCRYPT_MODE and str(p)[-3:] == 'kpk':
-				exit('\n Error: Can NOT encrypt ' + str(p) + '\n')
-
-			password = getPassword(mode)
-			
-			p_size = os.stat(p).st_size
-
-			if mode == ENCRYPT_MODE:
-				(key, salt) = deriveKey(password, None)
-				print('\n # Encrypting...\n')
-
-				(encryptor, iv) = aesEncryptor(key)
-				encryptor = writeMeta(encryptor, p, iv, salt)
-				encFile(encryptor, p, p_size)
-
-				if rm:
-					os.remove(p)
-			else:
-				print('\n # Decrypting...\n')
-
-				(decryptor, f_out_ext) = readMeta(p, password)
-				decFile(decryptor, p, p_size, f_out_ext)
-
-				if rm:
-					os.remove(p)
-
-		showProgressComplete()
-
-	else:
+	if mode == None:
 		exit('\n' + USAGE + '\n')
+		
+	p= Path(getPath(argv))
+	if p == None:
+		exit('\n Error: <path> variable NOT specified\n')
+	elif not os.path.exists(p):
+		exit('\n Error: Can NOT find ' + str(p) + '\n')
+
+	if os.path.isdir(p):
+		# Get all file paths in the directory recursively
+		fList = getFilesList(p, mode)
+		if len(fList) < 1:
+			exit('\n Error: ' + str(p) + ' is empty\n')
+
+		password = getPassword(mode)
+
+		if mode == ENCRYPT_MODE:
+			print('\n # Encrypting...\n')
+		else:
+			print('\n # Decrypting...\n')
+			
+		# Get total size of files in the directory
+		total_size = getTotalSize(fList)
+
+		if mode == ENCRYPT_MODE:
+			(key, salt) = deriveKey(password, None)
+			for f in fList:
+				if os.path.isdir(f):
+					continue
+				
+				(encryptor, iv) = aesEncryptor(key)
+				encryptor = writeMeta(encryptor, f, iv, salt)
+				encryptFile(encryptor, f, total_size)
+
+				if rm:
+					os.remove(f)
+		else:
+			for f in fList:
+				if os.path.isdir(f):
+					continue
+
+				(decryptor, f_out_ext) = readMeta(f, password)
+				decryptFile(decryptor, f, total_size, f_out_ext)
+
+				if rm:
+					os.remove(f)
+	elif os.path.isfile(p):
+		if mode == ENCRYPT_MODE and str(p)[-3:] == 'kpk':
+			exit('\n Error: Can NOT encrypt ' + str(p) + '\n')
+
+		password = getPassword(mode)
+		
+		p_size = os.stat(p).st_size
+
+		if mode == ENCRYPT_MODE:
+			(key, salt) = deriveKey(password, None)
+			print('\n # Encrypting...\n')
+
+			(encryptor, iv) = aesEncryptor(key)
+			encryptor = writeMeta(encryptor, p, iv, salt)
+			encryptFile(encryptor, p, p_size)
+
+			if rm:
+				os.remove(p)
+		else:
+			print('\n # Decrypting...\n')
+
+			(decryptor, f_out_ext) = readMeta(p, password)
+			decryptFile(decryptor, p, p_size, f_out_ext)
+
+			if rm:
+				os.remove(p)
+
+	showProgressComplete()
 
 if __name__ == '__main__':
 	try:
