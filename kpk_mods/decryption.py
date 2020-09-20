dfa16504933f2e6ce7142c1d67cdec1a0909ae79
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2020-09-20 19:17:35 +0430

    refactored decryption module

--- kpk_mods/decryption.py
+++ kpk_mods/decryption.py
@@ -8,33 +8,43 @@ from cryptography.hazmat.primitives import padding
 
 from kpk_mods.passwd import deriveKey
 from kpk_mods.progress import showProgress, calcPercentage
-from kpk_mods.file_exntension import getFileExt, replaceFileExt
+from kpk_mods.file_exntension import replaceFileExt
 from kpk_mods.constants import BUFFER_SIZE
 
-def decryptFile(decryptor, f_in_path, total_size, f_out_ext):
-	f_out_path = replaceFileExt(f_in_path, f_out_ext)
+class Decryptor():
+	def __init__(self, password, f_in):
+		meta = self.readMeta(f_in)
+		(key, _) = deriveKey(password, meta['salt'])
+		self.decryptor = self.aesDecryptor(key, meta['iv'])
+		f_out_ext = self.decryptExt(meta['c_ext'])
+		self.f_out = replaceFileExt(f_in, f_out_ext)
+		self.f_in = f_in
+		self.f_in_size = stat(f_in).st_size - 48
 
-	f_in_size = stat(f_in_path).st_size - 48
-	with open(f_in_path, 'rb') as f_in:
-		f_in.read(48)
-		r_byte = f_in.read(BUFFER_SIZE)
+	def decryptFile(self, total_size):
+		with open(self.f_in, 'rb') as f_in:
+			f_in.read(48)
+			r_byte = f_in.read(BUFFER_SIZE)
 
-		with open(f_out_path, 'wb') as f_out:
-			unpadder = padding.PKCS7(128).unpadder()
-			if f_in_size < BUFFER_SIZE:
-				w_byte = decryptor.update(r_byte)
-				try:
-					w_byte = unpadder.update(w_byte) + unpadder.finalize()
-				except ValueError:
-					pass
-				f_out.write(w_byte)
+			with open(self.f_out, 'wb') as f_out:
+				unpadder = padding.PKCS7(128).unpadder()
+				if self.f_in_size < BUFFER_SIZE:
+					w_byte = self.decryptor.update(r_byte)
+					try:
+						w_byte = unpadder.update(w_byte) + unpadder.finalize()
+					except ValueError:
+						pass
+					f_out.write(w_byte)
 
-				percentage = calcPercentage(f_in_size, total_size)
-				showProgress(percentage)
-			else:
-				for i in range(1, ceil(f_in_size / BUFFER_SIZE) + 1):
-					w_byte = decryptor.update(r_byte)
-					if i == ceil(f_in_size / BUFFER_SIZE):
+					percentage = calcPercentage(self.f_in_size, total_size)
+					showProgress(percentage)
+
+					f_out.write(self.decryptor.finalize())
+					return
+
+				for i in range(1, ceil(self.f_in_size / BUFFER_SIZE) + 1):
+					w_byte = self.decryptor.update(r_byte)
+					if i == ceil(self.f_in_size / BUFFER_SIZE):
 						try:
 							w_byte = unpadder.update(w_byte) + unpadder.finalize()
 						except ValueError:
@@ -45,39 +55,32 @@ def decryptFile(decryptor, f_in_path, total_size, f_out_ext):
 					showProgress(percentage)
 
 					r_byte = f_in.read(BUFFER_SIZE)
+				f_out.write(self.decryptor.finalize())
 
-			f_out.write(decryptor.finalize())
+	def decryptExt(self, c_ext):
+		ext = self.decryptor.update(c_ext)
+		if len(split(b'%kapak%', ext)) < 2:
+			raise Exception(' Error: Invalid password\n')
+		f_out_ext = str(split(b'%kapak%', ext)[1], 'utf-8')
+		return f_out_ext
 
-def readMeta(f_in_path, password):
-	f_in_ext = getFileExt(f_in_path)
-	if f_in_ext != 'kpk':
-		raise Exception(' Error: Can NOT decrypt ' + str(f_in_path) + '\n')
+	def readMeta(self, f_in_path):
+		# Extract iv, salt, encrypted extension
+		f_in = open(f_in_path, 'rb')
+		iv = f_in.read(16)
+		salt = f_in.read(16)
+		c_ext = f_in.read(16)
+		f_in.close()
+		return {
+			'iv': iv,
+			'salt': salt,
+			'c_ext': c_ext
+		}
 
-	# Extract iv, salt, encrypted extension
-	f_in = open(f_in_path, 'rb')
-	iv = f_in.read(16)
-	salt = f_in.read(16)
-	c_ext = f_in.read(16)
-	f_in.close()
-
-	# Derive the key
-	(key, _) = deriveKey(password, salt)
-
-	decryptor = aesDecryptor(key, iv)
-
-	ext = decryptor.update(c_ext)
-
-	if len(split(b'%kapak%', ext)) < 2:
-		raise Exception(' Error: Invalid password\n')
-
-	f_out_ext = str(split(b'%kapak%', ext)[1], 'utf-8')
-
-	return (decryptor, f_out_ext)
-
-def aesDecryptor(key, iv):
-	cipher = Cipher(
-		algorithms.AES(key), 
-		modes.CBC(iv), 
-		backend=default_backend()
-	)
-	return cipher.decryptor()
+	def aesDecryptor(self, key, iv):
+		cipher = Cipher(
+			algorithms.AES(key), 
+			modes.CBC(iv), 
+			backend=default_backend()
+		)
+		return cipher.decryptor()
