062fb9b1d1e44697b9758a35c7f223a2fe8aa52d
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2020-12-21 21:35:55 +0330

    Added zip functions

--- lib/dir.py
+++ lib/dir.py
@@ -1,13 +1,15 @@
-from os import path, stat
+import os
+import pathlib
+import zipfile
 
-from lib.constants import ENCRYPT_MODE
+from lib.constants import ENCRYPT_MODE, TEMP_ZIP_EXT
 from lib.file_exntension import fileExt
 
 def calcTotalSize(files):
 	totalSize = 1
 	for f in files:
-		if path.isfile(f):
-			totalSize += stat(f).st_size
+		if os.path.isfile(f):
+			totalSize += os.stat(f).st_size
 	return totalSize
 
 def files(dir_path, mode):
@@ -15,8 +17,23 @@ def files(dir_path, mode):
 		files = []
 		allFiles = list(dir_path.glob('**/*'))
 		for f in allFiles:
-			if path.isfile(f) and fileExt(f) != 'kpk':
+			if os.path.isfile(f) and fileExt(f) != 'kpk':
 				files.append(f)
 		return files
 
 	return list(dir_path.glob('**/*.kpk'))
+
+def zipDir(dir_path):
+	ff = list(dir_path.glob('**/*'))
+	zp = pathlib.Path(
+		os.path.split(dir_path.resolve())[0],
+		dir_path.name+'.'+TEMP_ZIP_EXT
+	)
+	with zipfile.ZipFile(zp, 'w') as zf:
+		for f in ff:
+			zf.write(f)
+	return zp
+
+def unzipDir(dir_path):
+	with zipfile.ZipFile(dir_path, 'r') as zf:
+		zf.extractall()
