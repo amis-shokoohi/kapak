b3f31bd1798ef5218cd121dc4a1a260a2b842be8
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2021-02-06 20:30:21 +0330

    Used stdlib argparse to parse subcommand flags

--- cmd_/decrypt.py
+++ cmd_/decrypt.py
@@ -1,64 +1,69 @@
 import os
+from pathlib import Path
+import argparse
 
 from lib.message import print_help_decrypt
-from lib.argparse import has_remove_flag, get_path
 from lib.file_exntension import file_ext
 from lib.passwd import get_password
 from lib.constants import DECRYPT_MODE, TEMP_ZIP_EXT
 from lib.progress import Progress
-from lib.decryptor import FileDecryptor
+import lib.decryptor
 from lib.dir import unzip_dir, calc_total_size, list_files
 
-def execute(argv):
+def execute(argv: [str]):
 	if len(argv) == 2 or argv[2] == '-h' or argv[2] == '--help':
 		print_help_decrypt()
 		return
 
-	should_remove = True if has_remove_flag(argv[2:]) else False
+	parser = argparse.ArgumentParser(prog='kapak', add_help=False)
+	subparser = parser.add_subparsers()
+	subparser_decrypt_cmd = subparser.add_parser('decrypt', add_help=False)
+	subparser_decrypt_cmd.add_argument('-r', '--remove', action='store_true', dest='should_remove')
+	subparser_decrypt_cmd.add_argument('path', type=Path)
+	args = subparser_decrypt_cmd.parse_args(args=argv[2:])
 
-	target_path = get_path(argv[2:])
-	if target_path == None:
-		raise Exception('PATH is not specified')
-	if not os.path.exists(target_path):
-		raise Exception('can not find ' + str(target_path))
+	if not os.path.exists(args.path):
+		raise Exception('can not find ' + str(args.path))
 
-	if os.path.isfile(target_path):
-		if file_ext(target_path) != 'kpk':
+	if os.path.isfile(args.path):
+		decrypt_file(args.path, args.should_remove)
+	elif os.path.isdir(args.path):
+		decrypt_dir(args.path, args.should_remove)
+
+def decrypt_file(target_path: Path, should_remove: bool):
+	if file_ext(target_path) != 'kpk':
 			raise Exception('can not decrypt ' + str(target_path))
-		target_size = os.stat(target_path).st_size
-		if target_size == 0:
-			raise Exception(str(target_path) + ' is empty')
 
-		password = get_password(DECRYPT_MODE)		
+	target_size = os.stat(target_path).st_size
+	if target_size == 0:
+		raise Exception(str(target_path) + ' is empty')
 
-		print('\nDecrypting...\n')
-		progress = Progress()
-		progress.set_total_size(target_size)
-		decryptor = FileDecryptor(password, target_path)
-		decryptor.decrypt()
-		if decryptor.get_file_ext() == TEMP_ZIP_EXT:
-			unzip_dir(decryptor.get_file_name())
-			os.remove(decryptor.get_file_name())
+	password = get_password(DECRYPT_MODE)		
+
+	print('\nDecrypting...\n')
+	progress = Progress()
+	progress.set_total_size(target_size)
+	f_out_path, f_out_ext = lib.decryptor.decrypt(password, target_path)
+	if f_out_ext == TEMP_ZIP_EXT:
+		unzip_dir(f_out_path)
+		os.remove(f_out_path)
+
+	if should_remove:
+		os.remove(target_path)
+
+def decrypt_dir(target_path: Path, should_remove: bool):
+	password = get_password(DECRYPT_MODE)
+
+	print('\nDecrypting...\n')
+	ff = list_files(target_path, DECRYPT_MODE) # List of files in the directory
+	if len(ff) == 0:
+		raise Exception(str(target_path) + ' is empty')
+	target_size = calc_total_size(ff)
+	if target_size == 0:
+		raise Exception(str(target_path) + ' is empty')
+	progress = Progress()
+	progress.set_total_size(target_size)
+	for f in ff:
+		_, _ = lib.decryptor.decrypt(password, f)
 		if should_remove:
-			os.remove(target_path)
-		print('\r' + 20*' ' + '\r[■■■■■■■■■■] 100%\n') # TODO: This is a hack, should be fixed in lib/progress.py
-		return
-
-	if os.path.isdir(target_path):
-		password = get_password(DECRYPT_MODE)		
-
-		print('\nDecrypting...\n')
-		ff = list_files(target_path, DECRYPT_MODE) # List of files in the directory
-		if len(ff) == 0:
-			raise Exception(str(target_path) + ' is empty')
-		target_size = calc_total_size(ff)
-		if target_size == 0:
-			raise Exception(str(target_path) + ' is empty')
-		progress = Progress()
-		progress.set_total_size(target_size)
-		for f in ff:
-			decryptor = FileDecryptor(password, f)
-			decryptor.decrypt()
-			if should_remove:
-				os.remove(f)
-		print('\r' + 20*' ' + '\r[■■■■■■■■■■] 100%\n') # TODO: This is a hack, should be fixed in lib/progress.py
+			os.remove(f)
--- cmd_/encrypt.py
+++ cmd_/encrypt.py
@@ -1,99 +1,105 @@
 import os
 import shutil
+from pathlib import Path
+import argparse
 
 from lib.message import print_help_encrypt
-from lib.argparse import has_remove_flag, has_zip_flag, get_path
 from lib.file_exntension import file_ext
 from lib.passwd import get_password, derive_key
 from lib.constants import ENCRYPT_MODE
 from lib.progress import Progress
-from lib.encryptor import FileEncryptor
+import lib.encryptor
 from lib.file_exntension import replace_file_ext
 from lib.dir import zip_dir, calc_total_size, list_files
 
-def execute(argv):
+def execute(argv: [str]):
 	if len(argv) == 2 or argv[2] == '-h' or argv[2] == '--help':
 		print_help_encrypt()
 		return
 
-	should_remove = True if has_remove_flag(argv[2:]) else False
-	should_zip = True if has_zip_flag(argv[2:]) else False
+	parser = argparse.ArgumentParser(prog='kapak', add_help=False)
+	subparser = parser.add_subparsers()
+	subparser_encrypt_cmd = subparser.add_parser('encrypt', add_help=False)
+	subparser_encrypt_cmd.add_argument('-z', '--zip', action='store_true', dest='should_zip')
+	subparser_encrypt_cmd.add_argument('-r', '--remove', action='store_true', dest='should_remove')
+	subparser_encrypt_cmd.add_argument('path', type=Path)
+	args = subparser_encrypt_cmd.parse_args(args=argv[2:])
 
-	target_path = get_path(argv[2:])
-	if target_path == None:
-		raise Exception('PATH is not specified')
-	if not os.path.exists(target_path):
-		raise Exception('can not find ' + str(target_path))
+	if not os.path.exists(args.path):
+		raise Exception('can not find ' + str(args.path))
 
-	if os.path.isfile(target_path):
-		if file_ext(target_path) == 'kpk':
-			raise Exception('can not encrypt ' + str(target_path))
-		f_out_name = replace_file_ext(target_path, 'kpk')
+	if os.path.isfile(args.path):
+		encrypt_file(args.path, args.should_remove)
+	elif args.should_zip and os.path.isdir(args.path):
+		zip_dir_then_encrypt(args.path, args.should_remove)
+	elif os.path.isdir(args.path):
+		encrypt_dir(args.path, args.should_remove)
+
+def encrypt_file(target_path: Path, should_remove: bool):
+	if file_ext(target_path) == 'kpk':
+		raise Exception('can not encrypt ' + str(target_path))
+
+	f_out_name = replace_file_ext(target_path, 'kpk')
+	if os.path.exists(f_out_name): # Overwrite error
+		raise Exception(str(f_out_name) + ' already exists')
+
+	target_size = os.stat(target_path).st_size
+	if target_size == 0:
+		raise Exception(str(target_path) + ' is empty')
+
+	password = get_password(ENCRYPT_MODE)
+
+	print('\nEncrypting...\n')
+	progress = Progress()
+	progress.set_total_size(target_size)
+	key, salt = derive_key(password, None)
+	lib.encryptor.encrypt(key, salt, target_path)
+
+	if should_remove:
+		os.remove(target_path)
+
+def zip_dir_then_encrypt(target_path: Path, should_remove: bool):
+	f_out_name = str(target_path) + '.kpk'
+	if os.path.exists(f_out_name): # Overwrite error
+		raise Exception(f_out_name + ' already exists')
+
+	password = get_password(ENCRYPT_MODE)
+
+	print('\nZipping...')
+	zp = zip_dir(target_path) # Creates a temporary zip file
+	target_size = os.stat(zp).st_size
+	if target_size == 0:
+		raise Exception(str(target_path) + ' is empty')
+
+	print('\nEncrypting...\n')
+	progress = Progress()
+	progress.set_total_size(target_size)
+	key, salt = derive_key(password, None)
+	lib.encryptor.encrypt(key, salt, zp)
+	os.remove(zp)
+
+	if should_remove:
+		shutil.rmtree(target_path)
+
+def encrypt_dir(target_path: Path, should_remove: bool):
+	password = get_password(ENCRYPT_MODE)
+
+	print('\nLooking for files in the directory...')
+	ff = list_files(target_path, ENCRYPT_MODE) # List of files in the directory
+	if len(ff) == 0:
+		raise Exception(str(target_path) + ' is empty')
+	target_size = calc_total_size(ff)
+	if target_size == 0:
+		raise Exception(str(target_path) + ' is empty')
+	
+	print('\nEncrypting...\n')
+	progress = Progress()
+	progress.set_total_size(target_size)
+	key, salt = derive_key(password, None)
+	for f in ff:
+		f_out_name = replace_file_ext(f, 'kpk')
 		if os.path.exists(f_out_name): # Overwrite error
 			raise Exception(f_out_name + ' already exists')
-		target_size = os.stat(target_path).st_size
-		if target_size == 0:
-			raise Exception(str(target_path) + ' is empty')
-
-		password = get_password(ENCRYPT_MODE)
-
-		print('\nEncrypting...\n')
-		progress = Progress()
-		progress.set_total_size(target_size)
-		key, salt = derive_key(password, None)
-		encryptor = FileEncryptor(key, salt, target_path)
-		encryptor.encrypt()
+		lib.encryptor.encrypt(key, salt, f)
 		if should_remove:
-			os.remove(target_path)
-		print('\r' + 20*' ' + '\r[■■■■■■■■■■] 100%\n') # TODO: This is a hack, should be fixed in lib/progress.py
-		return
-
-	if should_zip and os.path.isdir(target_path):
-		f_out_name = str(target_path) + '.kpk'
-		if os.path.exists(f_out_name): # Overwrite error
-			raise Exception(f_out_name + ' already exists')
-
-		password = get_password(ENCRYPT_MODE)
-
-		print('\nZipping...')
-		zp = zip_dir(target_path) # Creates a temporary zip file
-		target_size = os.stat(zp).st_size
-		if target_size == 0:
-			raise Exception(str(target_path) + ' is empty')
-
-		print('\nEncrypting...\n')
-		progress = Progress()
-		progress.set_total_size(target_size)
-		key, salt = derive_key(password, None)
-		encryptor = FileEncryptor(key, salt, zp)
-		encryptor.encrypt()
-		os.remove(zp)
-		if should_remove:
-			shutil.rmtree(target_path)
-		return
-
-	if os.path.isdir(target_path):
-		password = get_password(ENCRYPT_MODE)
-
-		print('\nLooking for files in the directory...')
-		ff = list_files(target_path, ENCRYPT_MODE) # List of files in the directory
-		if len(ff) == 0:
-			raise Exception(str(target_path) + ' is empty')
-		target_size = calc_total_size(ff)
-		if target_size == 0:
-			raise Exception(str(target_path) + ' is empty')
-		
-		print('\nEncrypting...\n')
-		progress = Progress()
-		progress.set_total_size(target_size)
-		key, salt = derive_key(password, None)
-		for f in ff:
-			f_out_name = replace_file_ext(f, 'kpk')
-			if os.path.exists(f_out_name): # Overwrite error
-				raise Exception(f_out_name + ' already exists')
-			encryptor = FileEncryptor(key, salt, f)
-			encryptor.encrypt()
-			if should_remove:
-				os.remove(f)
-		print('\r' + 20*' ' + '\r[■■■■■■■■■■] 100%\n') # TODO: This is a hack, should be fixed in lib/progress.py
-		
+			os.remove(f)
