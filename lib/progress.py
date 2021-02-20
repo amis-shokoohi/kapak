0f1e988f230f4d1706c999776d9a258f76d7806f
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2021-02-21 00:23:56 +0330

    Progress for GUI

--- lib/progress.py
+++ lib/progress.py
@@ -1,4 +1,5 @@
 from math import ceil, floor
+from typing import Callable
 
 class Progress:
 	__instance = None
@@ -16,12 +17,14 @@ class Progress:
 		'[■■■■■■■■■■]'
 	]
 
-	def __init__(self):
+	def __init__(self, is_gui=False):
 		if Progress.__instance:
 			raise Exception("not able to create another instance from Progress")
 		Progress.__instance = self
 		self.__processed_bytes_len = 0
 		self.__total_bytes_len = 0
+		self.__percentage = 0
+		self.__is_gui = is_gui
 
 	@staticmethod
 	def get_instance():
@@ -32,11 +35,22 @@ class Progress:
 	def set_total_size(self, total_bytes_len: int):
 		self.__total_bytes_len = total_bytes_len
 
+	def set_print_fn(self, print_fn: Callable):
+		self.__print_gui = print_fn
+
 	def update(self, bytes_len: int):
 		self.__processed_bytes_len += bytes_len
+		p = ceil(self.__processed_bytes_len / self.__total_bytes_len * 100)
+		self.__percentage = p if p <= 100 else 100
 
 	def print(self):
-		p = ceil(self.__processed_bytes_len / self.__total_bytes_len * 100)
-		percentage = p if p <= 100 else 100
-		i = floor(percentage / 10)
-		print('\r' + 20*' ' + '\r' + self.__bar[i] + ' ' + str(percentage) + '%', end='')
+		if self.__is_gui:
+			self.__print_gui(self.__percentage)
+			return
+		i = floor(self.__percentage / 10)
+		print('\r' + 20*' ' + '\r' + self.__bar[i] + ' ' + str(self.__percentage) + '%', end='')
+	
+	def reset(self):
+		self.__processed_bytes_len = 0
+		self.__total_bytes_len = 0
+		self.__percentage = 0
