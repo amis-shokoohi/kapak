f7f07d0a3375ff1df085bb05e7812d09fdcb1d01
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2021-02-21 00:21:36 +0330

    Implemented logging

--- view/util.js
+++ view/util.js
@@ -1,53 +1,71 @@
-export function displayFolderOptions(tab) {
-	return e => {
-		if (e.target.checked)
-			tab.isFile = false;
-		tab.pathEl.value = 'No folder chosen...';
-		tab.zipOptionEl.style.display = 'flex';
-	}
-}
-
-export function hideFolderOptions(tab) {
-	return e => {
-		if (e.target.checked)
-			tab.isFile = true;
-		tab.pathEl.value = 'No file chosen...';
-		tab.zipOptionEl.style.display = 'none';
-	}
-}
-
-export function showBrowser(tab) {
-	return e => {
-		e.preventDefault();
-		if (tab.isFile) {
-			window.pywebview.api.open_file_dialog().then(result => {
-				alert(typeof result);
-				tab.pathEl.value = shortPath(result.toString('utf8'));
-			});
-		} else {
-			window.pywebview.api.open_folder_dialog().then(result => {
-				tab.pathEl.value = shortPath(result.toString('utf8'));
-			});
-		}
+function showBrowser(e) {
+	e.preventDefault();
+	if (this.fileRadioEl.checked) {
+		window.pywebview.api.open_file_dialog().then(result => {
+			this.pathEl.value = shortPath(result.toString('utf8'));
+		});
+	} else {
+		window.pywebview.api.open_folder_dialog().then(result => {
+			this.pathEl.value = shortPath(result.toString('utf8'));
+		});
 	}
 }
 
 function shortPath(path) {
-	if (path.length > 29) {
-		const sub = path.substring(14, path.length - 14);
+	if (path.length > 27) {
+		const sub = path.substring(13, path.length - 13);
 		return path.replace(sub, '...');
 	}
 	return path;
 }
 
-export function showError(msg) {
-	const duration = 3000;
-	const card = document.createElement('div');
-	card.innerText = msg;
-	card.classList.add('err-card');
-	const logo = document.getElementById('logo');
-	document.body.insertBefore(card, logo);
-	setTimeout(() => card.style.top = '3rem', 100);
-	setTimeout(() => card.style.top = '-3rem', duration);
-	setTimeout(() => card.remove(), duration + 200);
+function disableForm() {
+	const disableEl = document.getElementById('disable');
+	disableEl.style.width = '100%';
+	disableEl.style.height = '100%';
+}
+
+function enableForm() {
+	const disableEl = document.getElementById('disable');
+	disableEl.style.width = '0%';
+	disableEl.style.height = '0%';
+}
+
+function logMsg(msg) {
+	const msgEl = document.getElementById('message');
+	msgEl.innerText = msg;
+}
+
+function logError(err) {
+	const errEl = document.getElementById('error');
+	errEl.innerText = 'Error: ' + err;
+}
+
+function showProgress(percentage) {
+	const logsDiv = document.querySelector('#logs > div');
+	logsDiv.innerHTML = `
+		<div id="progressbar" class="w-100">
+			<div role="progressbar" aria-valuenow="${percentage}" 
+				aria-valuemin="0" aria-valuemax="100" style="width:${percentage}%;">
+			</div>
+		</div>
+		<p id="percentage">${percentage}%</p>
+	`;
+}
+
+function clearLogs() {
+	const msgEl = document.getElementById('message');
+	msgEl.innerText = '';
+	const errEl = document.getElementById('error');
+	errEl.innerText = '';
+	const logsDiv = document.querySelector('#logs > div');
+	logsDiv.innerHTML = '';
+}
+
+function resetForm(id) {
+	const form = document.getElementById(id);
+	if (form.optionZip) {
+		form.optionZip.parentElement.style.display = 'none';
+	}
+	form.reset();
 }
