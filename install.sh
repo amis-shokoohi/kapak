48bccc6af66c851501cbf5971d5ed203c92ee7d7
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2021-02-22 19:43:26 +0330

    Updated install script

--- install.sh
+++ install.sh
@@ -1,45 +1,57 @@
 #! /usr/bin/env bash
 
-echo '#! /usr/bin/env python3' > kapak
-cat ./kapak.py >> kapak
+CURR_DIR=$(pwd)
+OPT_KPK_DIR=/opt/kapak
+BIN_DIR=/usr/local/bin
 
-CURRENT_DIR=$(pwd)
-ROOT_DIR=/root/kapak
-BIN_DIR=/bin/kapak
-
-if [[ $CURRENT_DIR != $ROOT_DIR ]]; then
-	if [[ -d $ROOT_DIR ]]; then
-		rm -rf $ROOT_DIR
-	fi
-
-	mkdir $ROOT_DIR
-	cp -r ${CURRENT_DIR}/* $ROOT_DIR
-	cd $ROOT_DIR
+if [[ ! -f ${CURR_DIR}/kapak.py ]]; then
+	echo "Error: You have to cd into kapak directory"
+	exit 1
 fi
 
-echo -e "\nFixing permissions..."
+echo -e "removing old kapak version...\n"
 sleep 2
-chmod +x ${ROOT_DIR}/install.sh
-chmod +x ${ROOT_DIR}/kapak
 
-if [[ -d $BIN_DIR ]]; then
-	rm -rf $BIN_DIR
+OLD_BIN_DIR=/bin/kapak
+if [[ -d $OLD_BIN_DIR ]]; then
+	rm -rf $OLD_BIN_DIR
 fi
 
-mkdir $BIN_DIR
-
-echo "Copying files to $BIN_DIR..."
+echo -e "Copying files to ${OPT_KPK_DIR}...\n"
 sleep 2
-cp -r ${ROOT_DIR}/* $BIN_DIR
-if [[ -f ${BIN_DIR}/kapak.py ]]; then
-	rm ${BIN_DIR}/kapak.py
+
+if [[ -d $OPT_KPK_DIR ]]; then
+	rm -rf $OPT_KPK_DIR
+fi
+mkdir $OPT_KPK_DIR
+
+cp -r ${CURR_DIR}/* $OPT_KPK_DIR
+
+echo '#! /usr/bin/env python3' > ${OPT_KPK_DIR}/kapak
+echo 'from sys import path' >> ${OPT_KPK_DIR}/kapak
+echo "path.insert(0, '${OPT_KPK_DIR}')" >> ${OPT_KPK_DIR}/kapak
+cat ${OPT_KPK_DIR}/kapak.py >> ${OPT_KPK_DIR}/kapak
+chmod +x ${OPT_KPK_DIR}/kapak
+rm ${OPT_KPK_DIR}/kapak.py
+
+echo '#! /usr/bin/env python3' > ${OPT_KPK_DIR}/kapak-gui
+echo 'from sys import path' >> ${OPT_KPK_DIR}/kapak-gui
+echo "path.insert(0, '${OPT_KPK_DIR}')" >> ${OPT_KPK_DIR}/kapak-gui
+cat ${OPT_KPK_DIR}/kapak-gui.py >> ${OPT_KPK_DIR}/kapak-gui
+chmod +x ${OPT_KPK_DIR}/kapak-gui
+rm ${OPT_KPK_DIR}/kapak-gui.py
+
+echo -e "Adding script to ${BIN_DIR}...\n"
+sleep 2
+
+if [[ -f ${BIN_DIR}/kapak ]]; then
+	rm ${BIN_DIR}/kapak
+fi
+if [[ -f ${BIN_DIR}/kapak-gui ]]; then
+	rm ${BIN_DIR}/kapak-gui
 fi
 
-rm -rf $ROOT_DIR
+ln -s ${OPT_KPK_DIR}/kapak ${BIN_DIR}/kapak
+ln -s ${OPT_KPK_DIR}/kapak-gui ${BIN_DIR}/kapak-gui
 
-echo "Adding kapak script to PATH..."
-sleep 2
-export PATH=${BIN_DIR}:$PATH
-echo "export PATH=${BIN_DIR}:$PATH" >> ~/.bashrc
 echo "Done"
-echo -e '\nOpen another terminal and type "kapak"\n'
