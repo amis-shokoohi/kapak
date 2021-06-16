955daf3602781fc9daa929b9143a269e6d2d408b
Amis Shokoohi <amisshokoohi@gmail.com>
2021-06-16 17:53:11 +0430

    Refactored

--- lib/passwd.py
+++ lib/passwd.py
@@ -1,43 +1,50 @@
 from pathlib import Path
 from getpass import getpass
 from sys import stderr
+from typing import Tuple
 
-from lib.constants import DECRYPT_MODE
 
-def get_password(mode: int) -> str:
-	password = None
+def get_password(confirm: bool) -> str:
+	password = ''
 	# Read the password from password.txt file
 	f = Path('password.txt')
 	if f.exists():
 		with open(f, 'r') as passwd_file:
-			password = str(passwd_file.read())
+			password = passwd_file.read().rstrip()
 		f.unlink()
+		is_correct, err_msg = _check_password_length(len(password))
+		if not is_correct:
+			raise Exception(err_msg)
 	else: # Prompt user to enter the password
-		password = _ask_pass(mode)
-	
-	while not _is_correct_pass_length(password):
-		password = _ask_pass(mode)
+		password = _ask_pass(confirm)
+		is_correct, err_msg = _check_password_length(len(password))
+		if not is_correct:
+			stderr.write('\nERROR: ' + err_msg + '\n\n')
+
+		while not is_correct:
+			password = _ask_pass(confirm)
+			is_correct, err_msg = _check_password_length(len(password))
+			if not is_correct:
+				stderr.write('\nERROR: ' + err_msg + '\n\n')
 
 	return password
 
-def _ask_pass(mode: int) -> str:
-	password1 = getpass('Enter password: ')
 
-	if mode == DECRYPT_MODE:
-		return password1
-		
-	password2 = getpass('Retype password: ')
-	while password1 != password2:
-		stderr.write('\nERROR: passwords do not match\n\n')
-		password1 = getpass('Enter password: ')
-		password2 = getpass('Retype password: ')
-	return password2
+def _ask_pass(confirm: bool) -> str:
+	p1 = getpass('Enter password: ')
 
-def _is_correct_pass_length(password: str) -> bool:
-	if len(password) < 3:
-		stderr.write('\nERROR: password should be at least 3 characters\n\n')
-		return False
-	elif len(password) > 1024:
-		stderr.write('\nERROR: password is to long\n\n')
-		return False
-	return True
+	if confirm:
+		p2 = getpass('Reenter password: ')
+		if p2 != p1:
+			stderr.write('\nERROR: passwords do not match\n\n')
+			return _ask_pass(True)
+
+	return p1
+
+
+def _check_password_length(pass_len: int) -> Tuple[bool, str]:
+	if pass_len < 3:
+		return False, 'password should be at least 3 characters'
+	elif pass_len > 1024:
+		return False, 'password is too long'
+	return True, ''
