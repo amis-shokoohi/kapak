b5e6f941e3bbf75e8bd799e093b8313c22f67713
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2023-01-24 20:50:07 +0330

    Fix aes tests version issue

--- tests/test_aes.py
+++ tests/test_aes.py
@@ -20,31 +20,31 @@ class Data:
 data_to_encrypt = [
     Data(
         input=b"yFwMtWN+oqXlKi",  # 14 bytes
-        expect=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAAChQeWV8Cchmg0sArLzwsek",
+        expect=b"oUHllfAnIZoNLAKy88LHpA==",
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
         input=b"ElOTSoqLIEg7KygP",  # 16 bytes
-        expect=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAADeG+Vf6g5fyEYP7CeyNlbW",
+        expect=b"3hvlX+oOX8hGD+wnsjZW1g==",
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
         input=b"vglB8G4IyL31vnt84A0V78+OkOPOu7",  # 30 bytes
-        expect=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAABzCQc7gH7WaZ2W+hoFxwyiF8MbLFwsWQHTztNO4MIzPw==",
+        expect=b"cwkHO4B+1mmdlvoaBccMohfDGyxcLFkB087TTuDCMz8=",
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
         input=b"BveQQwKR3TLa5KWOu4TiWLQIJ9gtYlvf",  # 32 bytes
-        expect=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAACPw+PuANmeBRSud+pC5FGCEujOIeKeiO/znxeAbKWcDA==",
+        expect=b"j8Pj7gDZngUUrnfqQuRRghLoziHinojv858XgGylnAw=",
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
         input=b"qTMHczZptvZHAsPCIeYXp03v+K1x76Qvv4",  # 34 bytes
-        expect=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAABhjOJCTYcelV0wB7kxhHU/cPPjHodUuf+cLf+h9fNWuRmASKQRbuAVm7wu6Xxn3T8=",
+        expect=b"YYziQk2HHpVdMAe5MYR1P3Dz4x6HVLn/nC3/ofXzVrkZgEikEW7gFZu8Lul8Z90/",
         password="P@ssw0rd",
         buffer_size=16,
     ),
@@ -67,8 +67,8 @@ def test_encrypt(monkeypatch: pytest.MonkeyPatch, data: Data) -> None:
 
     assert header.read(5) == b"kapak"
 
-    srcmajor_version = int.from_bytes(header.read(4), "big")
-    assert srcmajor_version == int(__version__.split(".")[0])
+    src_major_version = int.from_bytes(header.read(4), "big")
+    assert src_major_version == int(__version__.split(".")[0])
 
     iv = header.read(kapak.aes.AES_BLOCK_SIZE)
     assert iv == kapak.aes.AES_BLOCK_SIZE * b"\x00"
@@ -91,36 +91,36 @@ def test_encrypt(monkeypatch: pytest.MonkeyPatch, data: Data) -> None:
     encrypted_data_length = len(encrypted_data[4 + header_length :])
     assert encrypted_data_length % kapak.aes.AES_BLOCK_SIZE == 0
 
-    assert base64.standard_b64encode(encrypted_data) == data.expect
+    assert base64.standard_b64encode(encrypted_data[4 + header_length :]) == data.expect
 
 
 data_to_decrypt = [
     Data(
-        input=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAAChQeWV8Cchmg0sArLzwsek",
+        input=b"AAAAbWthcGFrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAAChQeWV8Cchmg0sArLzwsek",
         expect=b"yFwMtWN+oqXlKi",  # 14 bytes
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
-        input=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAADeG+Vf6g5fyEYP7CeyNlbW",
+        input=b"AAAAbWthcGFrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAADeG+Vf6g5fyEYP7CeyNlbW",
         expect=b"ElOTSoqLIEg7KygP",  # 16 bytes
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
-        input=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAABzCQc7gH7WaZ2W+hoFxwyiF8MbLFwsWQHTztNO4MIzPw==",
+        input=b"AAAAbWthcGFrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAABzCQc7gH7WaZ2W+hoFxwyiF8MbLFwsWQHTztNO4MIzPw==",
         expect=b"vglB8G4IyL31vnt84A0V78+OkOPOu7",  # 30 bytes
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
-        input=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAACPw+PuANmeBRSud+pC5FGCEujOIeKeiO/znxeAbKWcDA==",
+        input=b"AAAAbWthcGFrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAACPw+PuANmeBRSud+pC5FGCEujOIeKeiO/znxeAbKWcDA==",
         expect=b"BveQQwKR3TLa5KWOu4TiWLQIJ9gtYlvf",  # 32 bytes
         password="P@ssw0rd",
         buffer_size=16,
     ),
     Data(
-        input=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAABhjOJCTYcelV0wB7kxhHU/cPPjHodUuf+cLf+h9fNWuRmASKQRbuAVm7wu6Xxn3T8=",
+        input=b"AAAAbWthcGFrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAABhjOJCTYcelV0wB7kxhHU/cPPjHodUuf+cLf+h9fNWuRmASKQRbuAVm7wu6Xxn3T8=",
         expect=b"qTMHczZptvZHAsPCIeYXp03v+K1x76Qvv4",  # 34 bytes
         password="P@ssw0rd",
         buffer_size=16,
@@ -131,6 +131,7 @@ data_to_decrypt = [
 @pytest.mark.parametrize("data", data_to_decrypt)
 def test_decrypt(monkeypatch: pytest.MonkeyPatch, data: Data) -> None:
     monkeypatch.setattr(kapak.aes, "urandom", lambda n: n * b"\x00")
+    monkeypatch.setattr(kapak.aes, "__version__", "0.0.0")
 
     decrypted_data = b""
     with BytesIO(base64.standard_b64decode(data.input)) as src, BytesIO() as dst:
@@ -143,13 +144,14 @@ def test_decrypt(monkeypatch: pytest.MonkeyPatch, data: Data) -> None:
 
 def test_decrypt_wrong_password(monkeypatch: pytest.MonkeyPatch) -> None:
     data = Data(
-        input=b"AAAAbWthcGFrAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAAChQeWV8Cchmg0sArLzwsek",
+        input=b"AAAAbWthcGFrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAABBOIB2Sf1bxlU6Lv8t8bRZIAAAAIGDHZk3R4+1yU9A/844W8c3N6Jh491AISA2ZpmQBHISUAAAAAAAAAAChQeWV8Cchmg0sArLzwsek",
         expect=b"yFwMtWN+oqXlKi",  # 14 bytes
         password="Wr0ngP@ssw0rd",
         buffer_size=16,
     )
 
     monkeypatch.setattr(kapak.aes, "urandom", lambda n: n * b"\x00")
+    monkeypatch.setattr(kapak.aes, "__version__", "0.0.0")
 
     with BytesIO(base64.standard_b64decode(data.input)) as src, BytesIO() as dst:
         with pytest.raises(KapakError, match=r"wrong password"):
