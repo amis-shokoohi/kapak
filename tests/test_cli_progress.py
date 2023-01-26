e5ad6b37570f38020acdcfa8d151b7b603636dda
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2023-01-26 18:49:05 +0330

    Use pytest capsys instead of contextlib

--- tests/test_cli_progress.py
+++ tests/test_cli_progress.py
@@ -1,34 +1,34 @@
-import io
-import contextlib
 import itertools
+
+import pytest
 
 from kapak.cli.progress import Progress
 
 
-def test_progress_update() -> None:
+def test_progress_update(capsys: pytest.CaptureFixture) -> None:
     total = 12
     progress = Progress(total)
 
-    captured = io.StringIO()
-    with contextlib.redirect_stderr(captured):
-        expect = 0
-        for i in itertools.repeat(2, int(total / 2)):
-            expect += i
-            progress.update(i)
-            assert progress.progress == expect
+    expect = 0
+    for i in itertools.repeat(2, int(total / 2)):
+        expect += i
+        progress.update(i)
+        assert progress.progress == expect
 
-            percentage = int(expect / total * 100)
-            assert captured.getvalue().find(f"{percentage}%") != -1
+        percentage = int(expect / total * 100)
+
+        captured = capsys.readouterr()
+        assert captured.err.find(f"{percentage}%") != -1
 
 
-def test_progress_total_zero() -> None:
+def test_progress_total_zero(capsys: pytest.CaptureFixture) -> None:
     total = 0
     progress = Progress(total)
 
-    captured = io.StringIO()
-    with contextlib.redirect_stderr(captured):
-        expect = 0
-        for i in itertools.repeat(2, 5):
-            progress.update(i)
-            assert progress.progress == expect
-            assert captured.getvalue() == ""
+    expect = 0
+    for i in itertools.repeat(2, 5):
+        progress.update(i)
+        assert progress.progress == expect
+
+        captured = capsys.readouterr()
+        assert captured.out == ""
