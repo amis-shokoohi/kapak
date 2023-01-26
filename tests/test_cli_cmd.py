61baf8eef54bf5fb086b25e312ceb3e874c1cf2d
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2023-01-26 18:49:16 +0330

    Add more tests

--- tests/test_cli_cmd.py
+++ tests/test_cli_cmd.py
@@ -1,10 +1,14 @@
-import io
-import contextlib
+import random
+from pathlib import Path
+
+import pytest
 
 import kapak.cli.cmd
+import kapak.cli.password
+from kapak.error import KapakError
 
 
-def test_run_print() -> None:
+def test_run_print(capsys: pytest.CaptureFixture) -> None:
     cases = [
         (
             ["cmd"],
@@ -49,7 +53,153 @@ def test_run_print() -> None:
     ]
 
     for (input, expect) in cases:
-        captured = io.StringIO()
-        with contextlib.redirect_stdout(captured):
-            kapak.cli.cmd.run(input)
-            assert captured.getvalue() == expect + "\n"
+        kapak.cli.cmd.run(input)
+
+        captured = capsys.readouterr()
+        assert captured.out == expect + "\n"
+
+
+def test_run_encrypt_decrypt(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(kapak.cli.password, "getpass", lambda msg: "P@ssw0rd")
+
+    temp_file1 = Path(tmp_path, "temp_file1")
+    temp_file2 = Path(tmp_path, "temp_file2")
+    temp_file3 = Path(tmp_path, "temp_file3")
+
+    expect = bytes(random.getrandbits(8) for _ in range(32))
+    temp_file1.write_bytes(expect)
+
+    kapak.cli.cmd.run(
+        [
+            "cmd",
+            "encrypt",
+            "-b",
+            "64",
+            "-o",
+            str(temp_file2),
+            str(temp_file1),
+        ]
+    )
+    kapak.cli.cmd.run(
+        [
+            "cmd",
+            "decrypt",
+            "-b",
+            "64",
+            "-o",
+            str(temp_file3),
+            str(temp_file2),
+        ]
+    )
+
+    assert temp_file3.read_bytes() == expect
+
+
+def test_run_encrypt_decrypt_password_file(tmp_path: Path) -> None:
+    temp_file_password = Path(tmp_path, "temp_file_password")
+    temp_file_password.write_text("P@ssw0rd")
+
+    temp_file1 = Path(tmp_path, "temp_file1")
+    temp_file2 = Path(tmp_path, "temp_file2")
+    temp_file3 = Path(tmp_path, "temp_file3")
+
+    expect = bytes(random.getrandbits(8) for _ in range(32))
+    temp_file1.write_bytes(expect)
+
+    kapak.cli.cmd.run(
+        [
+            "cmd",
+            "encrypt",
+            "-b",
+            "64",
+            "-p",
+            str(temp_file_password),
+            "-o",
+            str(temp_file2),
+            str(temp_file1),
+        ]
+    )
+    kapak.cli.cmd.run(
+        [
+            "cmd",
+            "decrypt",
+            "-b",
+            "64",
+            "-p",
+            str(temp_file_password),
+            "-o",
+            str(temp_file3),
+            str(temp_file2),
+        ]
+    )
+
+    assert temp_file3.read_bytes() == expect
+
+
+def test_run_encrypt_path_not_exist(
+    tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(kapak.cli.password, "getpass", lambda msg: "P@ssw0rd")
+
+    temp_file1 = Path(tmp_path, "temp_file1")
+    temp_file2 = Path(tmp_path, "temp_file2")
+
+    with pytest.raises(KapakError, match=r"not able to find"):
+        kapak.cli.cmd.run(
+            [
+                "cmd",
+                "encrypt",
+                "-b",
+                "64",
+                "-o",
+                str(temp_file2),
+                str(temp_file1),
+            ]
+        )
+
+
+def test_run_encrypt_path_not_file(
+    tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(kapak.cli.password, "getpass", lambda msg: "P@ssw0rd")
+
+    temp_dir = Path(tmp_path, "temp_dir")
+    temp_dir.mkdir()
+    temp_file2 = Path(tmp_path, "temp_file2")
+
+    with pytest.raises(KapakError, match=r"is not a file"):
+        kapak.cli.cmd.run(
+            [
+                "cmd",
+                "encrypt",
+                "-b",
+                "64",
+                "-o",
+                str(temp_file2),
+                str(temp_dir),
+            ]
+        )
+
+
+def test_run_encrypt_empty_file(
+    tmp_path: Path, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(kapak.cli.password, "getpass", lambda msg: "P@ssw0rd")
+
+    temp_file1 = Path(tmp_path, "temp_file1")
+    temp_file2 = Path(tmp_path, "temp_file2")
+
+    temp_file1.write_bytes(b"")
+
+    with pytest.raises(KapakError, match=r"is empty"):
+        kapak.cli.cmd.run(
+            [
+                "cmd",
+                "encrypt",
+                "-b",
+                "64",
+                "-o",
+                str(temp_file2),
+                str(temp_file1),
+            ]
+        )
--- tests/test_cli_pipe.py
+++ tests/test_cli_pipe.py
@@ -1,13 +1,25 @@
+import io
+import sys
+import random
 from pathlib import Path
+from dataclasses import dataclass
+from typing import BinaryIO
+
+import pytest
 
 from kapak.cli.pipe import Pipe
+
+
+@dataclass(frozen=True)
+class FakeStdIn:
+    buffer: BinaryIO
 
 
 def test_pipe_filein_fileout(tmp_path: Path) -> None:
     temp_filein = Path(tmp_path, "temp_filein")
     temp_fileout = Path(tmp_path, "temp_fileout")
 
-    expect = b"rYDaPST0EqAFMe0B"
+    expect = bytes(random.getrandbits(8) for _ in range(32))
     temp_filein.write_bytes(expect)
 
     with Pipe(temp_filein, temp_fileout) as pipe:
@@ -17,3 +29,20 @@ def test_pipe_filein_fileout(tmp_path: Path) -> None:
         pipe.dst.write(pipe.src.read())
 
     assert temp_fileout.read_bytes() == expect
+
+
+def test_pipe_stdin_stdout(
+    monkeypatch: pytest.MonkeyPatch, capsysbinary: pytest.CaptureFixture
+) -> None:
+    expect = bytes(random.getrandbits(8) for _ in range(32))
+
+    monkeypatch.setattr(sys, "stdin", FakeStdIn(buffer=io.BytesIO(expect)))
+
+    with Pipe(None, None) as pipe:
+        assert pipe.src_is_file is False
+        assert pipe.dst_is_file is False
+
+        pipe.dst.write(pipe.src.read())
+
+    captured = capsysbinary.readouterr()
+    assert captured.out == expect
