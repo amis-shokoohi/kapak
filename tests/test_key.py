b75c36f90537b46d41b00a6d79df12244c194c38
Amis Shokoohi <amirhossein.shokoohizade@gmail.com>
2023-09-30 15:30:10 +0330

    Minor change

--- tests/test_key.py
+++ tests/test_key.py
@@ -1,12 +1,10 @@
-import base64
-
 from kapak.key import derive_key
 
 
 def test_derive_key() -> None:
     password = "P@ssw0rd"
     salt = 16 * b"\x00"
-    expected_key = b"ddsmaX1zc8IxVYOTGRbAy2zk+ImlRiub6ZRzgZXc4rU="
+    expected_key = "75db26697d7373c2315583931916c0cb6ce4f889a5462b9be994738195dce2b5"
 
     key = derive_key(password, salt)
-    assert base64.standard_b64encode(key) == expected_key
+    assert key.hex() == expected_key
